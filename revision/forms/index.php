ef7e4df57cb993c6f56cc70d949b7ac23692a992
Gustavo Clay <gustavoclay@gmail.com>
2018-08-19 17:03:13 -0300

    Revisão, continuar da aula Curso de PHP 7 - Aula 34 - Formulários ()

---
+++ revision/forms/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Fomulário Teste</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <form action="dados.php" method="POST">
+        Nome: <input type="text" name="nome" id=""><br>
+        Email: <input type="email" name="email" id=""><br>
+        <button type="Submit">Enviar</button>
+    </form>
+
+
+</body>
+</html>
