d6e1fe89e36215c791326251179f5b7a39935052
Gustavo Clay <gustavoclay@gmail.com>
2018-08-20 13:10:13 -0300

    continuar a partir da aula, Curso de PHP 7 - Aula 37 - Upload de Arquivos

--- revision/forms/index.php
+++ revision/forms/index.php
@@ -13,25 +13,22 @@
         //array erros
         $erros = array();
         
-        //validações
-        if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)) {
-            $erros[] = "Idade precisa ser um inteiro";
-        }
-        
-        if (!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
-            $erros[] = "Email inválido";
+        //validações -sanitize
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+        if (!filter_var($idade, FILTER_VALIDATE_INT)) {
+            $erros = "Idade precisa ser um inteiro";
         }
 
-        if (!$peso = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)) {
-            $erros[] = "Peso preisa ser um float";
-        }
-        
-        if (!$ip = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP)) {
-            $erros[] = "IP inválido";
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $erros = "Email inválido";
         }
 
-        if (!$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)) {
-            $erros[] = "URL inválida";
+        $url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            $erros = "URL inválida";
         }
 
         //exibindo mensagens
@@ -48,11 +45,10 @@
 
 
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+        Nome: <input type="peso" name="nome" id=""><br>
         Idade: <input type="text" name="idade" id=""><br>
         Email: <input type="email" name="email" id=""><br>
-        Peso: <input type="peso" name="peso" id=""><br>
-        IP: <input type="ip" name="ip" id=""><br>
-        URL: <input type="url" name="url" id=""><br>
+        URL: <input type="text" name="url" id=""><br>
         <button type="submit" name="enviar_formulario">Enviar Dados</button>
     </form>
     <br>
