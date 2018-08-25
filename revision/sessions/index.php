0ce9ea61c8b96f2443e42ac4cf37e6e30e3ebef7
Gustavo Clay <gustavoclay@gmail.com>
2018-08-25 20:15:31 -0300

    acerto arquivo revision/sessions/index.php

--- revision/sessions/index.php
+++ revision/sessions/index.php
@@ -2,15 +2,9 @@
 
 session_start();
 
-<<<<<<< HEAD
-$_SESSION['cor'] = "Verde";
-$_SESSION['carro'] = "Veloster";
-
-=======
 $_SESSION['cor'] = 'verde';
 $_SESSION['carro'] = 'veloster';
 
 echo $_SESSION['cor'].'<br>'.$_SESSION['carro'].'<br>'.session_id();
 
 echo "<br><a href=home.php>home.php</a>";
->>>>>>> 510de433585d097d25717bcd2b6ce25ebb5add6d
