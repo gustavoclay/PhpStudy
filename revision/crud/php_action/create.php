5f271a8ce8de7be6728002ed0cdd9724ee7629e4
Gustavo Clay <gustavoclay@gmail.com>
2018-08-28 08:48:46 -0300

    Crud terminado, Aprendido sobre XSS Cross Site Scripiting, Continuar da aula Curso de PHP 7 - Aula 52 - $_COOKIE

--- revision/crud/php_action/create.php
+++ revision/crud/php_action/create.php
@@ -3,12 +3,21 @@
 session_start();
 // Conexão com o Banco de Dados
 require_once 'db_connect.php';
+//CLEAR
+function clear($input) {
+	global $connect;
+	//sql
+	$var = mysqli_escape_string($connect ,$input);
+	//XSS
+	$var = htmlspecialchars($var);
+	return $var;
+}
 
 if (isset($_POST['btn-cadastrar'])) {
-	$nome = mysqli_escape_string($connect, $_POST['nome']);
-	$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-	$email = mysqli_escape_string($connect, $_POST['email']);
-	$idade = mysqli_escape_string($connect, $_POST['idade']);
+	$nome = clear($_POST['nome']);
+	$sobrenome = clear($_POST['sobrenome']);
+	$email = clear($_POST['email']);
+	$idade = clear($_POST['idade']);
 
 	$sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 
