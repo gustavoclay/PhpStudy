e5ab9f9f37e59e0f02c31937eb5faa8519932b5b
Gustavo Clay <gustavoclay@gmail.com>
2018-08-28 10:45:38 -0300

    Encerrado o curso, Estudar PHP OO e MVC

---
+++ revision/classroom/09-cook.php
@@ -0,0 +1,6 @@
+<?php
+// setcookie('user', 'Rodrigo', time()+3600);
+// setcookie('email', 'rodrigo@bol.com', time()+3600);
+// setcookie('ultima_pesquisa', 'tenis adidas', time()+3600);
+
+var_dump($_COOKIE);
---
+++ revision/classroom/10-datas.php
@@ -0,0 +1,4 @@
+<?php
+//DATAS
+date_default_timezone_get('America/Sao_Paulo');
+echo date('d/m/Y H:i:s');
