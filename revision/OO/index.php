0db8359b5ebb93fe2cac73d43eb59b771edfa028
Gustavo Clay <gustavoclay@gmail.com>
2018-09-02 23:18:15 -0300

    continuar da aula Curso POO Teoria – 02a – O que é um Objeto?

---
+++ revision/OO/index.php
@@ -0,0 +1,14 @@
+html:<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Teste</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php
+	phpinfo();
+?>
+</body>
+</html>
