cd50e2f813a5f4ac9c3b19389856ab0f34bb64cf
Gustavo Clay <gustavoclay@gmail.com>
2018-09-04 23:21:03 -0300

    Continuar da aula "Curso POO PHP #05b - Exemplo Prático em PHP". Proximos passos: 	Estudar MVC 	Iniciar estudos Laravel 	Buscar mais conhecimento no Manual do PHP

--- revision/OO/ContaBanco.php
+++ revision/OO/ContaBanco.php
@@ -2,64 +2,116 @@
 
 class ContaBanco {
 	//ATRIBUTOS
-	private $numConta;
+	public $numConta;
 	protected $tipo;
-	public $dono;
-	public $saldo;
-	public $status;
+	private $dono;
+	private $saldo;
+	private $status;
+
+	//CONSTRUTOR
+	public function __construct() {
+		$this->setSaldo(0);
+		$this->setStatus(false);
+	}
 
 	//METODOS
-	private abrirConta() {
-
+	public function abrirConta($tipo) {
+		$this->setTipo($tipo);
+		$this->setStatus(true);
+		if ($this->tipo == "CC") {
+			$this->setSaldo(50);
+		} elseif ($this->tipo == "CP") {
+			$this->setSaldo(150);
+		}
 	}
 
-	private fecharConta() {
-
+	public function fecharConta() {
+		if ($this->getSaldo() > 0) {
+			echo "ERRO 001: Não é possível realizar esta operação. Conta com saldo positivo.";
+		} elseif ($this->getSaldo() < 0) {
+			echo "ERRO 002: Não é possível realizar esta operação. Conta com saldo negativo.";
+		} else {
+			$this.setStatus(false);
+		}
 	}
 
-	private despositar() {
-
+	public function despositar($valor) {
+		if ($this->getStatus() == true) {
+			$this->setSaldo($this->getSaldo() + $valor);
+		} else {
+			echo "ERRO 003: Não é possível realizar esta operação. Conta inativa.";
+		}
 	}
 
-	private sacar() {
-
+	public function sacar($valor) {
+		if ($this->getStatus() == true) {
+			if ($this->getSaldo() > $valor) {
+				$this->setSaldo($this->getSaldo() - $valor);
+			} else {
+				echo "ERRO 004: Não é possível realizar esta operação. Saldo insuficiente.";
+			}
+		} else {
+			echo "ERRO 005: Não é possível realizar esta operação. Conta inativa.";
+		}
 	}
 
-	private pagarMensal() {
-
+	public function pagarMensal() {
+		$valor = 0;
+		if ($this->getTipo() == "CC") {
+			$valor = 12;
+		} elseif ($this->getTipo() == "CP") {
+			$valor = 20;
+		}
+		if ($this->getStatus() == true) {
+			if ($this->getSaldo() > $valor) {
+				$this->setSaldo($this->getSaldo() - $valor);
+			} else {
+				echo "ERRO 006: Não é possível realizar esta operação. Saldo insuficiente ao cobrar mensalidade.";
+			}
+		} else {
+			echo "ERRO 005: Não é possível realizar esta operação. Conta inativa.";
+		}
 	}
 
 	//GETTERS SETTERS
-	public getNumConta() {
-
+	public function getNumConta() {
+		return $this->numConta;
 	}
 
-	public setNumConta() {
-
+	public function setNumConta($numConta) {
+		$this->numConta = $numConta;
 	}
 
-	public getTipo() {
-
+	public function getTipo() {
+		return $this->tipo;
 	}
 
-	public setTipo() {
-
+	public function setTipo($tipo) {
+		$this->tipo = $tipo;
 	}
 
-	public getDono() {
-
+	public function getDono() {
+		return $this->dono;
 	}
 
-	public setDono() {
-
+	public function setDono($dono) {
+		$this->dono = $dono;
 	}
 
-	public getSaldo() {
-
+	public function getSaldo() {
+		return $this->saldo;
 	}
 
-	public setSaldo() {
+	public function setSaldo($saldo) {
+		$this->saldo = $saldo;
+	}
 
+	public function getStatus() {
+		return $this->status;
+	}
+
+	public function setStatus($status) {
+		$this->status = $status;
 	}
 
 
